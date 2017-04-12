2668be58c37ca4f2724ef1e4fef48579f6ffff35
zhexiao <zhexiao@163.com>
2017-04-12 01:25:53 +0000

    add merge sort

--- Sort/merge_sort.py
+++ Sort/merge_sort.py
@@ -1,10 +1,56 @@
 """
-归并排序Merge sort
-把原始数组分成若干子数组,对每一个子数组进行排序,
-继续把子数组与子数组合并,合并后仍然有序,直到全部合并完,形成有序的数组
-把无序数组[6 2 4 1 5 9]分为三个子数组
-将三个子数组[2 6] [1 4] [5 9]两两合并排序
-[1 2 4 6] [5 9]继续两组两组合并
-[1 2 4 5 6 9]合并完毕,排序完毕
+归并排序是建立在归并操作上的一种有效的排序算法,
+该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。
+将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。
+若将两个有序表合并成一个有序表，称为二路归并。
+
+归并排序的算法我们通常用递归实现，先把待排序区间[s,t]以中点二分，接着把左边子区间排序，
+再把右边子区间排序，最后把左区间和右区间用一次归并操作合并成有序的区间[s,t]。
+
+一般用于对总体无序，但是各子项相对有序的数列
 """
 
+
+def merge(left, right):
+    """
+    对二分后的序列进行排序合并
+    :param left: 
+    :param right: 
+    :return: 排序后的序列
+    """
+
+    i, j = 0, 0
+    result = []
+    while len(left) > i and len(right) > j:
+        if right[j] >= left[i]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+
+def merge_sort(lists):
+    """
+    归并排序
+    :param lists: 需要排序的序列
+    :return: 排序后的序列
+    """
+    if len(lists) <= 1:
+        return lists
+
+    # 得到中间长度，将序列二分
+    mid_len = int(len(lists)/2)
+    left = merge_sort(lists[:mid_len])
+    right = merge_sort(lists[mid_len:])
+
+    # 将二分后的序列进行排序
+    return merge(left, right)
+
+sort_lists = [22, 11, 55, 33, 66, 123, 2, 123123, 2155, 1231231]
+print(merge_sort(sort_lists))
